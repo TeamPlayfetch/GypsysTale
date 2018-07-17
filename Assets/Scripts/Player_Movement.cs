e679ed38f6fe981c1bbbd1cf1a1a41ca29c9e062
zaneronaldtalbot <zaneronaldtalbot@gmail.com>
2018-07-17 16:58:49 +1000

    Initial creation of Git + first push

---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class Player_Movement : MonoBehaviour {
+
+	public float moveSpeed = 10.0f;
+	public float jumpForce = 8.0f;
+	public float gravity = 30.0f;
+	public float rotSpeed = 3.0f;
+
+	private Vector3 moveDir = Vector3.zero;
+	private CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = gameObject.GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (controller.isGrounded){
+			moveDir = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+			moveDir = transform.TransformDirection (moveDir);
+
+			moveDir *= moveSpeed;
+
+		}
+
+		if(XCI.GetButtonDown(XboxButton.A)){
+			moveDir.y = jumpForce;
+		}
+
+//		if (moveDir != Vector3.zero){
+//			transform.rotation = Quaternion.Slerp (transform.rotation, 
+//				Quaternion.LookRotation (moveDir), Time.deltaTime * rotSpeed);
+//		}
+
+		moveDir.y -= gravity * Time.deltaTime;
+
+		controller.Move (moveDir * Time.deltaTime);
+
+	}
+}
