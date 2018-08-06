fbe54ceb6f73f245d80041f7da87f0c27eea860e
zaneronaldtalbot <zaneronaldtalbot@gmail.com>
2018-08-06 16:45:35 +1000

    bone UI goes away after a couple of seconds

--- Assets/Scripts/Bones/BoneScore.cs
+++ Assets/Scripts/Bones/BoneScore.cs
@@ -11,10 +11,12 @@ public class BoneScore : MonoBehaviour {
 	// amount of points per bone collected
 	public float boneAdd = 1f;
 
+	public GameObject textActive;
+
 	public float boneScore = 0.0f;
 	// Use this for initialization
 	void Start () {
-	
+		textActive.SetActive (false);
 	}
 		
 	// Update is called once per frame
@@ -25,8 +27,14 @@ public class BoneScore : MonoBehaviour {
 		public void OnTriggerEnter (Collider other){
 			if (other.tag == "Bone"){
 			boneScore += boneAdd;
-
-
+			textActive.SetActive (true);
+			StartCoroutine (BoneTime ());
+		} else {
+			textActive.SetActive (false);
+		}
 	}
-}
+
+	IEnumerator BoneTime(){
+		yield return new WaitForSecondsRealtime (5);
+	}
 }
