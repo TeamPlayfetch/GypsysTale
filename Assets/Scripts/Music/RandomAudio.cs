a5f7e575154c0308cd445310e9c6d0b54d8436bd
zaneronaldtalbot <zaneronaldtalbot@gmail.com>
2018-09-10 12:28:26 +1000

    added background music and created a script that plays a random track from a list

---
+++ Assets/Scripts/Music/RandomAudio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAudio : MonoBehaviour {
+
+    public AudioSource audio;
+    public AudioClip[] musicList;
+
+    //Start random background music
+    void Start()
+    {
+        PlayRandomMusic();
+    }
+
+
+    //Check to see if audio is playing, and if not play a random track from the array
+    void Update()
+    {
+        if (!audio.isPlaying)
+        {
+            PlayRandomMusic();
+        }
+    }
+
+    //chooses a random song from the array of songs and plays it
+    void PlayRandomMusic()
+    {
+        audio.clip = musicList[Random.Range(0, musicList.Length)] as AudioClip;
+        audio.Play();
+        Debug.Log(audio.clip);
+    }
+
+}
