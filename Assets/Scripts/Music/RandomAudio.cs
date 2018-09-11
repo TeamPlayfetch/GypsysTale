a3e14d47d2c3271358bdf2a1c225c7fa45ad572e
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2018-09-11 16:20:26 +1000

    Commented and cleaned up RandomAudio and DestroyParticles scripts, removed ToggleTasks script as it is no longer needed.

--- Assets/Scripts/Music/RandomAudio.cs
+++ Assets/Scripts/Music/RandomAudio.cs
@@ -1,34 +1,77 @@
-﻿using System.Collections;
+﻿//--------------------------------------------------------------------------------------
+// Purpose: Play random song.
+//
+// Description: This script plays a random song from a public array of songs. It is to be
+// attached to any object that has an audio source.
+//
+// Author: Zane Talbot.
+//
+// Edited: Thomas Wiltshire.
+//--------------------------------------------------------------------------------------
+
+// using, etc
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomAudio : MonoBehaviour {
+//--------------------------------------------------------------------------------------
+// RandomAudio object. Inheriting from MonoBehaviour.
+//--------------------------------------------------------------------------------------
+public class RandomAudio : MonoBehaviour
+{
+    // AUDIO //
+    //--------------------------------------------------------------------------------------
+    // Title for this section of public values.
+    [Header("Audio:")]
 
-    public AudioSource audio;
-    public AudioClip[] musicList;
+    // public array of audio clips
+    [LabelOverride("Music List")] [Tooltip("An array of music to play durring the game, will be selected from this list at random.")]
+    public AudioClip[] m_aacMusicList;
+    //--------------------------------------------------------------------------------------
 
-    //Start random background music
-    void Start()
+    // PRIVATE VALUES //
+    //--------------------------------------------------------------------------------------
+    // private audio source.
+    private AudioSource m_asAudioSource;
+    //--------------------------------------------------------------------------------------
+
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    void Awake()
     {
+        // get the audiosource component of the object
+        m_asAudioSource = GetComponent<AudioSource>();
+
+        // Run random music selection function.
         PlayRandomMusic();
     }
 
-
-    //Check to see if audio is playing, and if not play a random track from the array
+    //--------------------------------------------------------------------------------------
+    // Update: Function that calls each frame to update game objects.
+    //--------------------------------------------------------------------------------------
     void Update()
     {
-        if (!audio.isPlaying)
+        // if audio is not playing
+        if (!m_asAudioSource.isPlaying)
         {
+            // run random music selection function.
             PlayRandomMusic();
         }
     }
 
-    //chooses a random song from the array of songs and plays it
+    //--------------------------------------------------------------------------------------
+    // PlayRandomMusic: Chooses a random song from the array of songs and plays it.
+    //--------------------------------------------------------------------------------------
     void PlayRandomMusic()
     {
-        audio.clip = musicList[Random.Range(0, musicList.Length)] as AudioClip;
-        audio.Play();
-        Debug.Log(audio.clip);
-    }
+        // get an audio clip from the msuic list array.
+        m_asAudioSource.clip = m_aacMusicList[Random.Range(0, m_aacMusicList.Length)] as AudioClip;
 
+        // Play the audio
+        m_asAudioSource.Play();
+
+        // Display which audio is playing in the console.
+        Debug.Log(m_asAudioSource.clip);
+    }
 }
--- Assets/Scripts/Particles/Destroy_Particles.cs
+++ Assets/Scripts/Particles/Destroy_Particles.cs
@@ -1,11 +1,34 @@
-﻿using System.Collections;
+﻿//--------------------------------------------------------------------------------------
+// Purpose: Destroy particle.
+//
+// Description: This script destroys a particle after it has finished playing, to be 
+// attached to an  object with a particle component.
+//
+// Author: Zane Talbot.
+//
+// Edited: Thomas Wiltshire.
+//--------------------------------------------------------------------------------------
+
+// using, etc
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destroy_Particles : MonoBehaviour {
+//--------------------------------------------------------------------------------------
+// Destroy_Particles object. Inheriting from MonoBehaviour. Destroys a particle after a 
+// set time.
+//--------------------------------------------------------------------------------------
+public class Destroy_Particles : MonoBehaviour
+{
+    //--------------------------------------------------------------------------------------
+    // initialization.
+    //--------------------------------------------------------------------------------------
+    IEnumerator Start()
+    {
+        // wait for 1 second after the duration of the particle
+        yield return new WaitForSeconds (GetComponent<ParticleSystem>().duration + 1.0f);
 
-	IEnumerator Start(){
-		yield return new WaitForSeconds (GetComponent<ParticleSystem> ().duration + 1.0f);
-		Destroy (gameObject);
+        // destroy gameobject
+        Destroy (gameObject);
 	}
 }
