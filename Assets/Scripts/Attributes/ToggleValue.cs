1be484e551174c79ee889068fd0a52048a0ecee3
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2018-10-16 15:43:47 +1100

    Finished the Swap Camera script with the basic features. Started work on a Start Camera script that changes between a few different cameras at the start of the game. Started development on a custom attribute but might delete.

---
+++ Assets/Scripts/Attributes/ToggleValue.cs
@@ -0,0 +1,39 @@
+﻿////--------------------------------------------------------------------------------------
+//// Purpose: Switch the main scene camera.
+////
+//// Description: 
+////
+//// Author: Thomas Wiltshire
+////--------------------------------------------------------------------------------------
+
+//// Using, etc
+//using UnityEngine;
+
+//// Check if using the editor.
+//#if UNITY_EDITOR
+//using UnityEditor;
+//#endif
+
+////--------------------------------------------------------------------------------------
+//// ToggleValue object. Inheriting from PropertyAttribute. Used for adding toggles for
+//// public values in the unity inspector.
+////--------------------------------------------------------------------------------------
+//public class ToggleValue : PropertyAttribute
+//{
+//    // public bool for the toggle of the value.
+//    public bool m_bToggle;
+
+//    //--------------------------------------------------------------------------------------
+//    // initialization.
+//    //--------------------------------------------------------------------------------------
+//    public ToggleValue(bool bToggle)
+//    {
+//        // set toggle
+//        this.m_bToggle = bToggle;
+//    }
+
+//    // Check if using the editor.
+//#if UNITY_EDITOR
+
+//#endif
+//}
