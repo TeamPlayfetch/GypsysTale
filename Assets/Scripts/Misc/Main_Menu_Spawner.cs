4e1564d5fc9c57b45e593a133a30f7ebe6e88dec
zaneronaldtalbot <zaneronaldtalbot@gmail.com>
2018-11-05 16:08:05 +1100

    environmental changes in level blockout, main menu changes to scene and added new meshes for amphitheatre/fishparticle

---
+++ Assets/Scripts/Misc/Main_Menu_Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main_Menu_Spawner : MonoBehaviour {
+
+
+    public GameObject player;
+
+    public GameObject[] spawnPoints;
+    GameObject chosenSpawn;
+
+	// Use this for initialization
+	void Start () {
+
+        RandSpawn();
+
+        Instantiate(player, chosenSpawn.transform.position, chosenSpawn.transform.rotation);
+
+	}
+
+    void RandSpawn()
+    {
+        chosenSpawn = spawnPoints[Random.Range(0, spawnPoints.Length)] as GameObject;
+    }
+
+}
